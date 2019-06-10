4dc011bfa61d72f030fff3d71fd8c9fdbab5d9e4
helenjjchen <helenjoychen@gmail.com>
2019-06-09 19:36:13 -0500

    help

--- data/data_cleaning.py
+++ data/data_cleaning.py
@@ -3,7 +3,8 @@ import pandas as pd
 
 def combine_data(csv1, csv2):
     df1 = pd.read_csv(csv1)
-    df2 = pd.read_csv(csv2, usecols=['id', 'review_scores_rating'])
+    df1.drop(['neighbourhood_group', 'neighbourhood'], axis=1)
+    df2 = pd.read_csv(csv2, usecols=['id', 'review_scores_rating', 'first_review', 'neighbourhood_cleansed'])
 
     f = df1.merge(df2, how='left', on='id')
 
@@ -12,8 +13,8 @@ def combine_data(csv1, csv2):
 
 __name__ = '__main__'
 if __name__ == '__main__':
-    csv1 = 'listings.csv'
-    csv2 = 'ratings.csv'
-    df = combine_data(csv1, csv2)
+    c1 = 'listings.csv'
+    c2 = 'listings_full.csv'
+    df = combine_data(c1, c2)
     df.to_csv('chicago_data.csv', index=False)
 
